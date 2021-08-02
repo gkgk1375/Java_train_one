9bbf138ba183765ceccafb46eb4137b7f5c01363
504 <504@DESKTOP-B0KVD9C>
2021-08-02 18:09:08 +0900

    chap16완 chap17진행

--- two/src/chap16/two/five/FromDirectoryExample.java
+++ two/src/chap16/two/five/FromDirectoryExample.java
@@ -10,9 +10,10 @@ public class FromDirectoryExample {
 
 	public static void main(String[] args)  throws IOException {
 		Path path = Paths.get("D:\\KYH-DATA\\git_data\\Java_train_one\\one\\src");
+		                         // 역슬래시의 경우 \\두개 이렇게 두개 입력해야함
 		Stream<Path> stream = Files.list(path);
-		stream.forEach(p->System.out.println(p.getFileName()));
-		
+		stream.forEach(p->System.out.println(p.getFileName()));//폴더명만 가능함
+		// p=서브디렉토리또는파일에해당하는Path객체|서브디렉토리이름 또는 파일이름 리턴
 
 	}
 
---
+++ two/src/chap17/two/one/AppMain.java
@@ -0,0 +1,15 @@
+package chap17.two.one;
+
+public class AppMain extends Application {
+	
+	@Override
+	public void start(Stage primaryStage)throws Exception {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
